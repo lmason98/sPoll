f21c01df917c8d279ef5d03f5a31efe4b4bfeb12
lmason98 <lmason98@gmail.com>
2024-02-08 15:19:04 -0800

    initial commit

---
+++ api/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+  echo "Waiting for postgres..."
+
+  while [ ! nc -z $SQL_HOST $SQL_PORT ]
+  do
+    sleep 0.1
+  done
+
+  echo "PostgresSQL Started"
+fi
+
+python manage.py flush --no-input
+python manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py createsuperuser --no-input
+
+exec "$@"
